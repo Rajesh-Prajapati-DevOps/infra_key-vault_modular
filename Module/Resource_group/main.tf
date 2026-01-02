6f4addeaab2751cd511c0b6f29d476140a3ea7a0
Rajesh Prajapati <aarprajapati3@gmail.com>
2026-01-02 15:36:20 +0530

    resource-group1

--- Module/Resource_group/main.tf
+++ Module/Resource_group/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rgkv" {
   for_each = var.rg_kv 
-  name     = var.rg_kv.name
-  location = var.rg_kv.location
+  name     = each.value.name
+  location = each.value.location
 
 }
